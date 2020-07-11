ede861344e1f7ff442a9c7124b68ef40c6aec0e9
Pedro Prieto <pedro.prieto003@gmail.com>
2020-07-10 23:14:28 -0400

    add book

--- api/models/bookshelfModel.js
+++ api/models/bookshelfModel.js
@@ -9,7 +9,28 @@ const findAllBookshelfsByUserId = async (userId) => {
 }
 
 const getAllBooksOfABookShelf = async (shelfId) => {
-    return db('userShelfBook as usb').where("usb.shelfId",'=',shelfId).join('userBook as ub','ub.id','=','usb.bookId').join("books as b", 'b.id','=','ub.id')
+    return db('userShelfBook as usb').where("usb.shelfId",'=',shelfId).join('userBook as ub','ub.id','=','usb.bookId').join("books as b", 'b.id','=','ub.id').join('readingStatuses as rs', 'rs.id','=','ub.readingStatusId').select([
+        'usb.shelfId',
+        'usb.bookId',
+        'rs.name as readingStatus',
+        'b.title',
+        'b.thumbnail'
+    ])
+}
+const insertBook = async(userShelfBook) => {
+    return db('userShelfBook').insert(userShelfBook)
+}
+const insert = async (bookshelf) => {
+    const [id] = await db('bookshelf').insert(bookshelf).select('id')
+    return findById
 }
 
-module.exports = {findAllBookshelfsByUserId,getAllBooksOfABookShelf}
+const findById = (id) => {
+    return db('bookshelf').where({id})
+}
+
+const remove = (id) => {
+    return db('bookshelf').del().where({id})
+}
+
+module.exports = {findAllBookshelfsByUserId,getAllBooksOfABookShelf,insert,findById,remove}
--- api/routes/bookshelf.js
+++ api/routes/bookshelf.js
@@ -4,8 +4,29 @@ const userProfileExists = require('../middleware/userProfileExists');
 const BookShelf = require("../models/bookshelfModel");
 const router = express.Router();
 
-// GET ALL BOOKSHELFS OF USER
-router.get('/:userId', [authRequired, userProfileExists], async function (req, res) {
+router.use(authRequired)
+
+router.get('/:userId', [userProfileExists], getAllBookShelfsOfAUser);
+router.post('/:userId', [userProfileExists], createABookShelf);
+router.post('/bookshelf/:bookshelfId/userbook/:userbookId', addABookToBookShelf)
+
+
+const createABookShelf = async (req, res) => {
+    try {
+        const bookshelf = {
+            name: req.body.name,
+            private: req.body.private || false,
+            profileId: req.user.id
+        }
+        const newBookShelf = await BookShelf.insert(bookshelf)
+        return res.json(201).json(newBookShelf)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({error: "Server Error"})
+    }
+  }
+
+const getAllBookShelfsOfAUser = async (req, res) => {
     try {
         const bookshelfsHash = {}
         const bookshelfs = await BookShelf.findAllBookshelfsByUserId(req.user.id);
@@ -22,13 +43,18 @@ router.get('/:userId', [authRequired, userProfileExists], async function (req, r
         console.log(error)
         return res.status(500).json({error: "Server Error"})
     }
-  });
+  }
 
-  router.post('/:bookshelfId', [authRequired], async function (req, res) {
+  const addABookToBookShelf = async (req, res) => {
     try {
-        const bookshelf = "bookshelvs"
+        const userShelfBook = {
+            bookId: req.params.userBookId,
+            shelfId: req.params.bookshelfId
+        }
+        return res.status(200).json(bookshelfs)
     } catch (error) {
         console.log(error)
         return res.status(500).json({error: "Server Error"})
     }
-  });
+  }
+
